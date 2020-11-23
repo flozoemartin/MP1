155b6fafd6d55d4f0069dccc2268ea7a91cc2ce4
Flo Martin <73466308+flozoemartin@users.noreply.github.com>
2020-11-23 11:02:00 +0000

    Update dysmen_ewas.sh

--- Code/Dysmenorrhea/Submission Scripts/dysmen_ewas.sh
+++ Code/Dysmenorrhea/Submission Scripts/dysmen_ewas.sh
@@ -8,4 +8,4 @@
 WORK_DIR="/newhome/ti19522/EWAS/alspac_menstruation_project"
 module add languages/R-3.6.3
 
-R CMD BATCH --no-save --no-restore '--args dysmen_ewas houseman Cells Pheno B 15up age_meth /newhome/ti19522/EWAS/alspac_menstruation_project' /newhome/ti19522/common_files/meffil_EWAS_script.r /newhome/ti19522/EWAS/alspac_menstruation_project/dysmen_ewas.out
+R CMD BATCH --no-save --no-restore '--args dysmen_ewas houseman Cells Pheno B 15up age_meth /newhome/ti19522/EWAS/alspac_menstruation_project' /newhome/ti19522/common_files/meffil_EWAS_script.r /newhome/ti19522/EWAS/alspac_menstruation_project/ewas_results/dysmen_ewas.out
