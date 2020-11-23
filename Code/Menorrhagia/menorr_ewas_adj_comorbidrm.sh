edaf6959d13d3a54e1dc1c6173e1618242f7e1bd
Flo Martin <73466308+flozoemartin@users.noreply.github.com>
2020-11-23 10:50:52 +0000

    Create menorr_ewas_adj_comorbidrm.sh

---
+++ Code/Menorrhagia/menorr_ewas_adj_comorbidrm.sh
@@ -0,0 +1,12 @@
+
+
+#!/bin/bash
+#
+#
+#PBS -l nodes=1:ppn=1,walltime=12:00:00
+
+WORK_DIR="/newhome/ti19522/EWAS/alspac_menstruation_project"
+module add languages/R-3.6.3
+
+R CMD BATCH --no-save --no-restore '--args menorr_ewas houseman Cells Pheno_comorbidrm B 15up age_meth,men_age_m,contraception_meth,mat_edu,mat_occ_class,reg_smoker,reg_drinker,comorbidity /newhome/ti19522/EWAS' /newhome/ti19522/common_files/meffil_EWAS_script.r /newhome/ti19522/EWAS/alspac_menstruation_project/menorr_ewas_adj_comorbidrm.out
+
