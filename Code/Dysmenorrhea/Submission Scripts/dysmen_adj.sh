28594da183f9020a0edf1b324fd3e2981046cb72
Flo Martin <73466308+flozoemartin@users.noreply.github.com>
2020-11-30 15:46:11 +0000

    Update and rename dysmen_ewas_adj.sh to dysmen_adj.sh

---
+++ Code/Dysmenorrhea/Submission Scripts/dysmen_adj.sh
@@ -0,0 +1,11 @@
+
+
+#!/bin/bash
+#
+#
+#PBS -l nodes=1:ppn=1,walltime=12:00:00
+
+WORK_DIR="/newhome/ti19522/EWAS/alspac_menstruation_project"
+module add languages/R-3.6.3
+
+R CMD BATCH --no-save --no-restore '--args dysmen_ewas houseman Cells Pheno B 15up age_meth,men_age_m,contraception_meth,mat_edu,reg_smoker,reg_drinker /newhome/ti19522/EWAS/alspac_menstruation_project' /newhome/ti19522/common_files/meffil_EWAS_script.r /newhome/ti19522/EWAS/alspac_menstruation_project/ewas_results/dysmen/dysmen_adj/dysmen_adj.out
