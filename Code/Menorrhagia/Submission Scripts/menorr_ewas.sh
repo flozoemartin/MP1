33c69bfbe8261d6c8d5b83e4f9c172219d7332ca
Flo Martin <73466308+flozoemartin@users.noreply.github.com>
2020-12-17 15:00:01 +0000

    Update menorr_ewas.sh

--- Code/Menorrhagia/Submission Scripts/menorr_ewas.sh
+++ Code/Menorrhagia/Submission Scripts/menorr_ewas.sh
@@ -1,9 +1,12 @@
 
 
 #!/bin/bash
-#
-#
-#PBS -l nodes=1:ppn=1,walltime=12:00:00
+
+#PBS -N menorr_ewas
+#PBS -o menorr_ewas_out
+#PBS -l select=1:ncpus=1:mem=100M
+#PBS -l walltime=12:00:00
+
 WORK_DIR="/home/ti19522/EWAS/alspac_menstruation_project"
 module add lang/r/4.0.3-bioconductor-gcc
 cd $WORK_DIR
