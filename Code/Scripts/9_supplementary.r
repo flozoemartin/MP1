32be3824786164fc79f5582aba723e1d870f4885
Flo Martin <73466308+flozoemartin@users.noreply.github.com>
2021-06-03 15:35:12 +0100

    Create 9_supplementary.r

---
+++ Code/Scripts/9_supplementary.r
@@ -0,0 +1,17 @@
+##############################################################################################################################################################
+#                                                                                                                                                            #
+#       Supplementary material for mini project 1 manuscript                                                                                                 #
+#       Author:         Flo Martin                                                                                                                           #
+#       Date started:   19/11/2020                                                                                                                           #
+#       Date finished:  03/06/2021                                                                                                                           #
+#                                                                                                                                                            #
+##############################################################################################################################################################
+
+setwd("/Users/ti19522/OneDrive - University of Bristol/Documents/PhD/Year 1/Mini Project 1/R Data/Data")
+load("mp1.Rda")
+load("traits.Rda")
+load("traits_comorbidrm.Rda")
+
+library(ggstance)
+library(ggplot2)
+library(dplyr)
