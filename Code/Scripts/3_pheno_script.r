c25cd26836ece71697437b916e9c29990977f86a
Flo Martin <73466308+flozoemartin@users.noreply.github.com>
2021-07-21 11:26:05 +0100

    Update 3_pheno_script.r

--- Code/Scripts/3_pheno_script.r
+++ Code/Scripts/3_pheno_script.r
@@ -11,7 +11,7 @@
 
 # Load in the phenofile generated from merging ALSPAC dataset with samplesheet
 getwd()
-setwd("/Users/ti19522/Downloads/bluecrystal_files/EWAS")
+setwd("/location/of/the/data")
 load("Pheno.Rda")
 
 # So, for each person, I need to derive the right covariate information at each timepoint: if time_code == TF3 use 15yr covariates and if time_code == F17 use
